9d2cb8c6a94b23cf37b2eaf998f0481406587e77
çağatay kılınç <107580070+klncgty@users.noreply.github.com>
2023-11-29 20:36:54 +0300

    Add files via upload

---
+++ miuul_reccomedation/debug.py
@@ -0,0 +1,117 @@
+kurslar=[{
+            "isim":"Git, GitHub, GitLab ve Jira ile Profesyonel İş Akış ve Süreç Yönetimi",
+            "açıklama":"Git, GitHub, GitLab ve Jira ile İş Akış Yönetimi, kodlama projelerini daha verimli bir şekilde yönetmek isteyen herkes içindir. Bu eğitimde; Git, GitHub ve GitLab gibi kod yönetim araçlarını kullanmayı öğrenerek projelerinizi daha iyi organize edebilecek, iş birliği yapma ve hataları çözme yeteneklerinizi geliştirebileceksiniz. Ayrıca, sürekli entegrasyon (continuous integration) ve sürekli dağıtım (continuous delivery) konularında pratiğe sahip olacak ve iş akışını daha verimli hale getireceksiniz.",
+            "tags":["git","Github","Gİtlab","Jira","iş akışı","Süreç Yönetimi","Versiyon kontrol"]
+         },
+         {
+             "isim":"Laravel SaaS Uygulaması Geliştirme",
+             "açıklama":"Günlük yaşantımızda oldukça fazla online hizmet kullanıyor ve bunlara sürekli ödeme yapıyoruz. Kullandığımız bu platformların arka planındaki adımları hiç merak ettiniz mi? Bu eğitimde bir web geliştirici olarak bu adımları ele alacağız. Tasarımından yazılımına kadar sıfırdan bir SaaS (Hizmet için Yazılım) projesi geliştiriyor olacağız. ",
+             "tags":["Laravel","SaaS","PHP","Frontend","Backend","Tailwind","Web Development"]
+         },
+         {
+             "isim":"Beautiful Soup ve Selenium ile Web Scrapping",
+             "açıklama":"Bu eğitim, web sitelerinden veri çekme, düzenleme, analiz etme ve bilgi çıkarma süreçlerini öğrenerek veriye dayalı kararlar almak ve rekabet avantajı elde etmek için verilere erişim yeteneğinizi en üst düzeye çıkaracak. Web kazıma konusundaki becerilerinizi geliştirerek, dijital dünyanın zengin veri kaynaklarından bilgi toplama, analiz etme ve çıkartma süreçlerini öğreneceksiniz. Bu eğitimle gelecekte veri odaklı projelerinizi hızlandırabileceksiniz.",
+             "tags":["HTML","Xpath","Beautiful Soup","Selenium","HML","CSS","JavaScript","Bot Tespiti","Proxy"]
+         },
+         {
+             "isim":"Robotik Kodlama",
+             "açıklama":"Bu eğitim, Arduino tabanlı sistemlerle çalışma konusunda geniş bir bilgi ve beceri seti kazandırır ve teknoloji alanında rekabette bir adım öne çıkmanızı sağlar. Programlama, temel elektronik, devre kurulumu, çeşitli sensör ve aktüatörlerin kullanımı gibi alanlarda pratik deneyim kazanarak, Arduino'nun temel komutlarından karmaşık uygulamalara kadar geniş bir yelpazede bilgi edinirsiniz. Motorlar, sensörler, LED'ler, LCD ekranlar gibi çeşitli elektronik bileşenleri kullanarak gerçek dünya projeleri oluşturma yeteneğinizi geliştirirsiniz. Program bitiminde Arduino ve ilişkili teknolojileri kullanarak, ilgili projeler geliştirebilir, sorunları çözebilir ve yenilikçi fikirler üretebilirsiniz.",
+             "tags":["Robotik Kodlama","Arduino","Temel Elektronik","Analitik Düşünce"]
+         },
+         {
+             "isim":"Veri Bilimi ve Makine Öğrenmesi İçin Lineer Cebir",
+             "açıklama":"Günümüz iş dünyasında, Veri Bilimi ve Makine Öğrenmesi, veriye dayalı kararlar alarak rekabet avantajı elde etmek için kritik bir rol oynuyor. Lineer Cebir, bu alandaki temel matematiksel kavramları içerir ve veri analizi, model oluşturma, özellik mühendisliği gibi süreçlerde önemli bir rol oynar. Lineer Cebir'i öğrenmek, Veri Bilimi ve Makine Öğrenmesi başarısında kilit bir faktör olabilir. Bu eğitim, sizi Veri Bilimi ve Makine Öğrenmesi dünyasının heyecan verici bir kariyerine hazırlayacak ve size rekabet avantajı sağlayacak.",
+             "tags":["Lineer Cebir","Matris","Lineer denklem sistemleri","Python","Makine Öğrenmesi","Derin Öğrenme"]
+         },
+         {
+             "isim":"Django REST Framework ile API Geliştirme",
+             "açıklama":"Esneklikle mükemmeliyetçiliği buluşturan Django REST Framework eğitimi zengin içerik ve güçlü araçlarla her alana hitap eden modern API'lar oluşturabileceğiniz sağlam bir temel sunuyor.  Python'ın dinamizmi ve Django'nun esnek mimarisini harmanlayarak, uçtan uca API geliştirme sürecini deneyimleyeceksiniz. Bu yolculuk, sizi sadece bir geliştirici olmaktan öteye taşıyacak, teknoloji dünyasının stratejik bir oyuncusu yapacak.",
+             "tags":["Django","Django REst framework (DRF)","API"]
+         },
+         {
+             "isim":"Veri Bilimi İçin Python Programlama",
+             "açıklama":"Bu eğitim Python programlama dilini etkin bir PyCharm IDE kullanımı ile fonksiyonel yetenekler kapsamında öğrenilmesini sağlar. Veri okuma, veri ön işleme, özellik geliştirme ve makine öğrenmesi (machine learning) modelleri üretimi gibi tüm temel veri bilimi işlemlerini yapabilmeniz için gerekli altyapıyı proje geliştirme düzeyinde edineceksiniz.",
+             "tags":["Python","Veri analizi","Veri Maniulasyonu","Veri Görselleştirme","Numpy","Pandas"]
+         },
+         {
+             "isim":"Linux Temelleri ve Komut Satırı İşlemleri",
+             "açıklama":"Bu eğitim içeriği ile Linux tabanlı sistemlerin sanal bir makine ile kurulmasını, dosya sistemi hiyerarşisini ve temel komutlarının öğrenilmesini, dosya ve dizin erişimlerinin listelenmesini ve değiştirilmesini, sistemin izlenilmesini, paket yönetimlerinin sağlanılması, kullanıcı ve grup konfigürasyonlarının oluşturulmasını öğrenebileceğiniz gerekli altyapıyı kazanma şansı elde edeceksiniz.",
+             "tags":["Linux","Operating Systems","Commands"]
+         },
+         {
+             "isim":"MS SQL Sorgulama",
+             "açıklama":"MS SQL eğitimi, Microsoft SQL Server üzerinde derin bir anlayış geliştirmek isteyenlere yöneliktir. Bu eğitim, veritabanı tasarımı, sorgu optimizasyonu ve güvenlik konularında kapsamlı bilgi sağlar. Katılımcılar, veritabanı uygulamalarını başarılı bir şekilde yönetebilirler. Ayrıca, yedekleme, geri yükleme ve performans izleme gibi önemli konularda pratik beceriler kazanarak, MS SQL Server ortamında veritabanı yönetiminde uzmanlaşırlar.",
+             "tags":["Veri tabanı işlemleri","SQL komutları","Normalizasyon","Veri Sorgulama","JOIN türleri","Aggregate Fonksiyonları","İlişkisel Veritabanları"]
+         },
+         {
+             "isim":"CRM Analitiği",
+             "açıklama":"CRM Analitiği çalışmaları sektörde kendine en fazla yer bulan iş uygulamalarındandır. Python ile müşteri verilerini analiz etmek, müşterileri daha yakından tanımak, müşterileri segmentlere ayırmak, segmentlere özel iş kararları alabilmek ve müşterileri elde tutmak için müşteri terk modelleri geliştirme konuları ele alacağız. Programlama ve veri analizi kabiliyetlerinizi gerçek iş problemleri ile pekiştirme şansı edineceksiniz.",
+             "tags":["Müşteri analitiği","Müşteri segmentasyonu","Müşteri yaşam boyu tahmin değeri","CRM","RFM Analizi"]
+         },
+         {
+             "isim":"Ölçümleme Problemleri",
+             "açıklama":"Analistlerin nicel (quantitative) ölçümleri nasıl oluşturup değerlendirebilecekleri konusunda sıklıkla karşılarına çıkan birçok karmaşık kavramı düşünün. Bu eğitimde, bu kavramları ölçmek için çeşitli yaklaşımları keşfedecek; farklı ölçüm seviyelerine ve değişkenleri dönüştürmenin yollarına genel bir bakış edineceksiniz. Eğitimin sonunda, çalışmaya değer kavramlar için ölçütler geliştirebilecek ve eleştirel olarak değerlendirebileceksiniz. Sonuçta, iyi bir analiz, iyi ölçümler üzerine kuruludur.",
+             "tags":["rating products","sorting products","sorting reviews","A/B test"]
+         },
+         {
+             "isim":"Tavsiye Sistemleri",
+             "açıklama":"Tavsiye sistemleri Netflix, Facebook, Amazon, Spotify, Pinterest ve Instagram gibi teknoloji devlerinin başarılarında anahtar rolü oynuyor. Bu nedenle, veri bilimi dünyasında kritik bir yer tutan tavsiye sistemlerini kapsamlı şekilde ele alan bu modülde önemli bir yetkinlik kazandırmaktadır. Bu eğitimde tavsiye sistemlerinin teknik arka planını anlayacak ve Python ile tavsiye sistemi geliştirme seviyesine çıkacaksınız.",
+             "tags":["Association Rule Learning","Apriori ALgoritması","İçeri temelli Filtreleme","işbirlikçi filtreleme","metin vektörleştirme","matrix factorization"]
+         },
+         {
+             "isim":"Zaman Serileri",
+             "açıklama":"Zaman içerisinde sıralanmış bir dizi gözlem noktasından oluşan zaman serilerinde amaç; geçmişten gelen gözlemlerin mevsimsellik, trend ve durağanlık özelliklerini kullanarak gelecek için bir tahminde bulunmaktır. Finans, hava durumu tahminleri, satış tahminleri gibi alanlarda sıkça kullanılmaktadır.\
+                            Bu eğitimde işletmelerin ya da şahısların stratejik kararlar alırken önemli ölçüde kullandığı bu yöntem ile ilgili gerekli altyapının uygulamalar üzerinden aktarılması hedeflenmektedir.",
+             "tags":["LightGBM","ARIMA","SARIMA","İstatistiksel ve ekonometrik Zaman Serileri","Zaman serileri modeleme"]
+         },
+         {
+             "isim":"Özellik Mühendisliği",
+             "açıklama" :"Veri bilimi süreçlerinin çok büyük bir kısmını, veri temizliği ve değişken mühendisliği oluşturmaktadır. Bu eğitim; ham veriyi temizleyerek, ilgili veriden yeni özellik çıkarmak ve modele girmeye uygun olmayan değişkenleri dönüştürmek için öğrencilerin belirli yaklaşımlar edinmesini amaçlamaktadır. Edinilen yaklaşımlar sayesinde veri setini bir makine öğrenmesi (machine learning) modeline girmeye uygun hale getirme konusunda gerekli altyapıyı edineceksiniz.",
+             "tags":["Veri Önişleme","eksik değer analizi","aykırı değer analizi","one-hot&label encoding","özellik ölçeklendirme","özellik çıkarımı"],
+         },
+         {
+             "isim":"Makine Öğrenmesi",
+             "açıklama":"Veri bilimi Python programlama dilini kullanarak ve CRISP-DM süreçlerini dikkate alarak denetimli ve denetimsiz makine öğrenmesi (machine learning) yöntemlerini öğrenmemizi sağlar. Bu eğitimde geleneksel algoritmalar ve son zamanlarda öne çıkan birçok algoritmayı (XGBoost, LightGBM, CatBoost) kıyaslayarak ve model seçme özellikleri ile işleyerek önemli bir yetkinlik edineceksiniz.",
+             "tags":["CART","KNN","LightGBM","XGBoost","CatBoost","Hiperparametre optimizasyonu","Supervised learning","unsupervised learning","gradient descent","Machine learning","makine öğrenmesi"]
+         },
+         {
+             "isim":"Doğal Dil  işlemeye Giriş",
+             "açıklama":"Doğal Dil İşleme (NLP) eğitimi doğal dillerin yapısının çözümlenerek anlaşılması için gerekli olan text analitik süreçlerini ele alır. Chatbotlar, duygu analizleri veya çeviri gibi birçok uygulamanın temelini oluşturan algoritmaları Python dili kullanılarak geliştirebileceksiniz.",
+             "tags":["text processing","text visualization","sentiment analysis","word embeddings","TF-IDF","CountVectorization"]
+         },
+         {
+             "isim":"Büyük Veri Temeller",
+             "açıklama":"Büyük veri, geleneksel yaklaşımla işlenip yönetilemeyecek kadar büyük ve karmaşık veriye işaret eder. Veri günümüzde şirket ve kurumların çalışma şekillerini kökten değiştirmeye başlamıştır. Bu durum önümüzdeki yıllarda daha da hayati bir noktaya gelecek. Veriyi stratejik bir değer olarak gören şirketler görmeyenlere göre öne geçecektir.\
+                            Neredeyse iki yılda bir üretilen verinin ikiye katlandığı bir dünyada artık veriyi tek bir makineye sıkışarak işlemek, saklamak, analiz etmek zorlaşmaktadır. Üstelik artık her şeyin tek bir makinede olduğu sistemler tek nokta kırılganlığı taşıdığından veri işleyen sistemler ister istemez ölçeklenmektedir.\
+                        Bu modülde büyük verinin başlangıcından beri var olan ve halen dünya büyük verisinin çoğunu saklayan ve işleyen açık kaynak kodlu projeler uygulama seviyesinde öğrenilecektir. Aynı zamanda büyük veri ve dağıtık veri işlemenin temel mantığı özümsenecektir.",
+            "tags":["Big Data","Hadoop","Hİve","Kafka","Streaming","Sqoop","YARN"]
+         },
+         {
+             "isim":"Spark Makine Öğrenmesi",
+             "açıklama":"Spark, dağıtık veri işlemenin zorlu tarafını geliştiriciden yalıtarak ona bütünleşik ve kullanımı kolay API’ler sunan analitik bir veri işleme platformudur. Spark'ın bütünleşik programlama modeli, veri açısından zengin analitik uygulamalar geliştirenler için onu en iyi seçim haline getirir. Spark ile parti veya akan veri işlemek mümkündür.\
+                            Spark'ın onlarca farklı özelliklerinden bir tanesi da makine öğrenmesi modellerini büyük ölçekteki veriler üzerinde mümkün kılmasıdır. Bu eğitimde Spark ML kütüphaneleri tanınacak ve bu kütüphaneler yardımıyla supervised, unsupervised öğrenme yöntemleriyle örnek modeller geliştirilecektir.",
+            "tags":["Spark Machine Learning","Classification","Clustering","Regression","ML pipelines"]
+         },
+         {
+             "isim":"Production Level of Data Science",
+             "açıklama":"Bu eğitim 'Veri Bilimcileri' ve adaylarını teknik yönden güçlendirmek, onları konteyner ortamlarıyla tanıştırmak, farklı model dağıtım seçenekleri hakkında bilgiyle donatarak yeterli pratikliği kazandırmayı hedeflemektedir. Linux komut satırı temelleriyle başlayan teknik yeterliliği arttırma serüveni bash scripting, sürüm kontrol sistemi kullanımı, PostgreSQL temel ve konteyner ile devam etmektedir. Temel yetkinliği kazandıktan sonra geliştirilen modellerin dağıtımı ve otomasyonu konusunda uygulama seviyesinde yetkinlik kazanılacaktır.",
+             "tags":["linux","Bash Script","Model Deployment","Docker","Kubernetes","MLFlow"]
+         }]
+
+
+
+tags_gpt = ["Git", "Web Development", "Python", "Machine Learning", "Data Science"] 
+
+found_courses = []  # Benzer kelimeleri içeren kursları tutacak liste
+
+for kurs in kurslar:
+    for tag in kurs["tags"]:
+        for gpt_tag in tags_gpt:
+            if gpt_tag.lower() in tag.lower():
+                found_courses.append(kurs["isim"])
+
+for course in found_courses:
+    print(course)
+
+
+
